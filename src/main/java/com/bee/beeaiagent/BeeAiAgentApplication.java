7e9f18bcd6964f887424a72c17fd58c89e06dcaa
Bee mines <Beemines82@gmail.com>
2026-05-07 10:59:58 +0800

    后端项目初始化文件

---
+++ src/main/java/com/bee/beeaiagent/BeeAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.bee.beeaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeeAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bee/beeaiagent/BeeAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bee.beeaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeeAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
